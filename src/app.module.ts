c9b390fb5e29dc11a58c92526ebb2f5c2290adb1
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-11-02 14:11:54 -0400

    feat(api): add initial module imports for Student, Config, and Mongoose.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentModule } from './student/student.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    StudentModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/student'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
