cc580984190c93161ace1bb99d429b83c9652c26
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-11-02 15:12:06 -0400

    feat(api): define CreateStudentDto and UpdateStudentDto with properties

--- src/student/dto/create-student.dto.ts
+++ src/student/dto/create-student.dto.ts
@@ -1,1 +1,10 @@
-export class CreateStudentDto {}
+export class CreateStudentDto {
+  code!: number;
+  name!: string;
+  lastname!: string;
+  age!: number;
+  email!: string;
+  password!: string;
+  phone!: string;
+  career!: string;
+}
--- src/student/dto/update-student.dto.ts
+++ src/student/dto/update-student.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateStudentDto } from './create-student.dto';
 
-export class UpdateStudentDto extends PartialType(CreateStudentDto) {}
+export class UpdateStudentDto extends PartialType(CreateStudentDto) {
+  code!: number;
+  name?: string;
+  lastname?: string;
+  age?: number;
+  email?: string;
+  password?: string;
+  phone?: string;
+  career?: string;
+}
