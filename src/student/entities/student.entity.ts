e3f6125d1f9a7c484144c7425aa166fe4b3b7ab5
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-11-02 15:11:13 -0400

    feat(api): define Student entity with properties and schema.

--- src/student/entities/student.entity.ts
+++ src/student/entities/student.entity.ts
@@ -1,1 +1,36 @@
-export class Student {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type StudentDocument = Student & Document;
+
+@Schema({ collection: 'computing' })
+export class Student {
+  @Prop({ type: Types.ObjectId })
+  id: Types.ObjectId;
+
+  @Prop({ required: true, type: Number })
+  code: number;
+
+  @Prop({ required: true, type: String, name: 'full_name' })
+  name: string;
+
+  @Prop({ required: true, type: String, name: 'last_name' })
+  lastname: string;
+
+  @Prop({ required: true, type: Number, name: 'age' })
+  age: number;
+
+  @Prop({ required: true, unique: true, type: String, name: 'email' })
+  email: string;
+
+  @Prop({ required: true, type: String, name: 'password' })
+  password: string;
+
+  @Prop({ required: true, type: String, name: 'phone' })
+  phone: string;
+
+  @Prop({ required: true, type: String, name: 'career' })
+  career: string;
+}
+
+export const StudentSchema = SchemaFactory.createForClass(Student);
